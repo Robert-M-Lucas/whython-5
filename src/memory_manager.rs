69b157803e530d583a25bced0ef6f4915ccb4893
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-15 15:21:17 +0100

    Created new memory system to work with dynamic memory allocation paradigm of Whython-5

--- src/memory_manager.rs
+++ src/memory_manager.rs
@@ -1,6 +1,9 @@
+use std::collections::LinkedList;
 use num_format::{Locale, ToFormattedString};
 use std::fs;
 use std::io::Write;
+use std::ops::Index;
+use std::slice::SliceIndex;
 
 #[derive(Default)]
 pub struct MemoryManager {
@@ -78,6 +81,152 @@ impl MemoryManager {
         let r = file.write_all(&self.memory);
         if r.is_err() {
             println!("Failed to write to file - {}", r.unwrap_err())
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum MemoryLocation {
+    Program,
+    Stack,
+    Heap,
+}
+
+pub struct StackMemory {
+    memory: LinkedList<(Vec<u8>, usize)>,
+    current_stack: usize,
+}
+
+impl StackMemory {
+    pub fn create_stack(&mut self, size: usize, return_addr: usize) {
+        self.memory.push_back((vec![0; size], return_addr));
+    }
+    
+    pub fn index(&self, mut position: usize) -> u8 {
+        for m in self.memory.iter().skip(self.current_stack) {
+            if position >= m.0.len() { position -= m.0.len(); continue; }
+            return m.0[position];
+        }
+
+        panic!("Index out of stack!");
+    }
+
+    pub fn index_slice(&self, mut start: usize, mut end: usize) -> &[u8] {
+        for m in self.memory.iter().skip(self.current_stack) {
+            if start >= m.0.len() { start -= m.0.len(); end -= m.0.len(); continue; }
+            return &m.0[start..end];
+        }
+
+        panic!("Index out of stack!");
+    }
+
+    pub fn get_stack(&self, mut position: usize) -> &[u8] {
+        for m in self.memory.iter().skip(self.current_stack) {
+            if position >= m.0.len() { position -= m.0.len(); continue; }
+            return &m.0;
+        }
+
+        panic!("Index out of stack!");
+    }
+}
+
+// impl Index<usize> for &StackMemory {
+//     type Output = u8;
+//
+//     fn index(&self, mut index: usize) -> &Self::Output {
+//         for m in self.memory.iter().rev() {
+//             if index <= m.len() { index -= m.len(); continue; }
+//             return &m[index];
+//         }
+//
+//         panic!("Index out of stack");
+//     }
+// }
+
+// impl<Idx> Index<Idx> for StackMemory
+//     where
+//         Idx: SliceIndex<[u8]>,
+// {
+//     type Output = Idx::Output;
+// 
+//     fn index(&self, index: Idx) -> &Self::Output {
+//         for m in self.memory.iter().skip(self.current_stack) {
+// 
+//         }
+// 
+//         ()
+//     }
+// }
+
+// impl<Idx> Index<Idx> for &StackMemory
+//     where
+//         Idx: SliceIndex<[u8]>,
+// {
+//     type Output = Idx::Output;
+//
+//     fn index(&self, index: Idx) -> &Self::Output {
+//
+//     }
+// }
+
+pub struct RuntimeMemoryManager {
+    program_memory: Vec<u8>,
+    stack_memory: StackMemory,
+    heap_memory: Vec<u8>
+}
+
+impl RuntimeMemoryManager {
+    pub fn program_memory(&self) -> &[u8] {
+        &self.program_memory
+    }
+
+    pub fn stack_memory(&mut self) -> &mut StackMemory {
+        &mut self.stack_memory
+    }
+
+    pub fn heap_memory(&mut self) -> &mut Vec<u8> {
+        &mut self.heap_memory
+    }
+
+    pub fn get_memory(&self, location: &MemoryLocation, start_position: usize) -> &[u8] {
+        match location {
+            MemoryLocation::Program => {
+                &self.program_memory
+            },
+            MemoryLocation::Stack => {
+                self.stack_memory.get_stack(start_position)
+            },
+            MemoryLocation::Heap => {
+                &self.heap_memory
+            }
+        }
+    }
+
+    pub fn get_data(&self, location: &MemoryLocation, address: usize, length: usize) -> &[u8] {
+        match location {
+            MemoryLocation::Program => {
+                &self.program_memory[address..address + length]
+            },
+            MemoryLocation::Stack => {
+                self.stack_memory.index_slice(address, address + length)
+            },
+            MemoryLocation::Heap => {
+                &self.heap_memory[address..address + length]
+            }
+        }
+    }
+
+    pub fn get_byte(&self, location: &MemoryLocation, address: usize) -> u8 {
+        match location {
+            MemoryLocation::Program => {
+                self.program_memory[address]
+            },
+            MemoryLocation::Stack => {
+                self.stack_memory.index(address)
+            },
+            MemoryLocation::Heap => {
+                self.heap_memory[address]
+            }
         }
     }
 }
