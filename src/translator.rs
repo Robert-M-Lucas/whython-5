2c2fe3f4fb77accd5f6cdd85efd47741435241ac
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-16 00:11:35 +0100

    Removed Whython-4 imports

--- src/translator.rs
+++ src/translator.rs
@@ -1,19 +1,3 @@
-use crate::processing::instructions::add_instruction_13::AddInstruction;
-use crate::processing::instructions::and_instruction_6::AndInstruction;
-use crate::processing::instructions::copy_instruction_0::CopyInstruction;
-use crate::processing::instructions::dynamic_from_copy_instruction_10::DynamicFromCopyInstruction;
-use crate::processing::instructions::dynamic_to_copy_instruction_11::DynamicToCopyInstruction;
-use crate::processing::instructions::equal_instruction_7::EqualInstruction;
-use crate::processing::instructions::input_instruction_15::InputInstruction;
-use crate::processing::instructions::invert_instruction_1::InvertInstruction;
-use crate::processing::instructions::jump_if_not_instruction_2::JumpIfNotInstruction;
-use crate::processing::instructions::jump_instruction_3::JumpInstruction;
-use crate::processing::instructions::jump_variable_instruction_4::JumpVariableInstruction;
-use crate::processing::instructions::not_equal_instruction_14::NotEqualInstruction;
-use crate::processing::instructions::or_instruction_8::OrInstruction;
-use crate::processing::instructions::print_chars_instruction_9::PrintCharsInstruction;
-use crate::processing::instructions::print_instruction_5::PrintInstruction;
-
 macro_rules! translate {
     ($instruction: ident, $data: expr, $i: expr) => {
         (
@@ -32,23 +16,7 @@ pub fn translate(data: &[u8], translate_one: bool) {
 
         let code = &data[i..i + 2];
         i += 2;
-        let (output, size) = match u16::from_le_bytes(code.try_into().unwrap()) {
-            0 => translate!(CopyInstruction, data, i),
-            1 => translate!(InvertInstruction, data, i),
-            2 => translate!(JumpIfNotInstruction, data, i),
-            3 => translate!(JumpInstruction, data, i),
-            4 => translate!(JumpVariableInstruction, data, i),
-            5 => translate!(PrintInstruction, data, i),
-            6 => translate!(AndInstruction, data, i),
-            7 => translate!(EqualInstruction, data, i),
-            8 => translate!(OrInstruction, data, i),
-            9 => translate!(PrintCharsInstruction, data, i),
-            10 => translate!(DynamicFromCopyInstruction, data, i),
-            11 => translate!(DynamicToCopyInstruction, data, i),
-            12 => translate!(JumpInstruction, data, i),
-            13 => translate!(AddInstruction, data, i),
-            14 => translate!(NotEqualInstruction, data, i),
-            15 => translate!(InputInstruction, data, i),
+        let (output, size): (String, usize) = match u16::from_le_bytes(code.try_into().unwrap()) {
             code => panic!("Debug not implemented for code {}", code),
         };
 
