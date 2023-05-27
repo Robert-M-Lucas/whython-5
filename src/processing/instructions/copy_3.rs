dfb04b4e648a8455a40d67f0a4386b85e35dcc2a
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-27 17:24:43 +0100

    Created copy instruction

---
+++ src/processing/instructions/copy_3.rs
@@ -0,0 +1,44 @@
+use crate::address::Address;
+use crate::processing::instructions::{Instruction, InstructionCode};
+use crate::util::get_usize;
+
+pub struct CopyInstruction {
+    address: usize
+}
+
+pub const COPY_INSTRUCTION_CODE: InstructionCode = 3;
+
+impl CopyInstruction {
+    pub fn new_alloc(memory_manager: &mut crate::memory::MemoryManager, address_from: &Address, address_to: &Address, size: usize) -> Self {
+        if address_to.is_immediate() {
+            panic!("Attempted to create CopyInstruction that overwrites Immediate memory!");
+        }
+
+        let size_bytes = size.to_le_bytes();
+        let mut from_bytes = address_from.get_bytes();
+        let mut to_bytes = address_to.get_bytes();
+        let mut instruction_memory = Vec::with_capacity(from_bytes.len() + to_bytes.len() + size_bytes.len());
+
+        instruction_memory.extend(size_bytes.iter());
+        instruction_memory.append(&mut from_bytes);
+        instruction_memory.append(&mut to_bytes);
+
+        let address = memory_manager.append(&instruction_memory);
+
+        Self { address }
+    }
+
+    #[allow(unused_variables)]
+    pub fn get_debug(memory: &[u8], pointer: &mut usize) -> String {
+        let size = get_usize(pointer, memory);
+        *pointer += Address::get_address_size(memory, *pointer, size);
+        *pointer += Address::get_address_size(memory, *pointer, size);
+        stringify!($name).to_string()
+    }
+}
+
+impl Instruction for CopyInstruction {
+    fn get_address(&self) -> usize {
+        self.address
+    }
+}
