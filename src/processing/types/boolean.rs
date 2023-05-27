f21bf9891e552eb90c3e0e3bd17c296ef16c4129
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-27 17:24:26 +0100

    Implemented allocate_variable and get_constant methods for boolean

--- src/processing/types/boolean.rs
+++ src/processing/types/boolean.rs
@@ -1,4 +1,11 @@
 use crate::{processing::symbols::{TypeSymbol, Operator}, default_type_wrapper_struct_and_impl, default_type_struct, default_type_initialiser, default_get_type_symbol_impl, default_type_operate_impl};
+use crate::address::Address;
+use crate::errors::create_literal_not_impl_error;
+use crate::memory::MemoryManager;
+use crate::processing::blocks::{BlockCoordinator, StackSizes};
+use crate::processing::instructions::copy_3::CopyInstruction;
+use crate::processing::symbols::Literal;
+use crate::util::warn;
 
 use super::{Operation, Type};
 
@@ -6,8 +13,50 @@ default_type_wrapper_struct_and_impl!(BoolWrapper, BoolType);
 default_type_struct!(BoolType);
 default_type_initialiser!(BoolType, BoolAnd);
 
+pub const BOOL_TRUE: u8 = 0xFF;
+pub const BOOL_FALSE: u8 = 0x00;
+
 impl Type for BoolType {
     default_get_type_symbol_impl!(BoolType, TypeSymbol::Boolean);
+
+    fn allocate_variable(&mut self, stack: &mut StackSizes, program_memory: &mut MemoryManager, to_assign: Option<&Literal>) -> Result<(), String> {
+        if self.address.is_some() { warn(format!("Allocating {} when it already has a memory address", self.get_type_symbol()).as_str()) }
+        self.address = Some(Address::StackDirect(stack.increment_stack_size(1)));
+
+        if let Some(literal) = to_assign {
+            let constant = self.get_constant(literal)?;
+            CopyInstruction::new_alloc(program_memory, &constant, self.address.as_ref().unwrap(), 1);
+        }
+        // ? If no literal memory will be default initialised to 0x00 (false)
+        
+        Ok(())
+    }
+
+    fn get_constant(&self, literal: &Literal) -> Result<Address, String> {
+        match literal {
+            Literal::Bool(value) => {
+                if *value {
+                    Ok(Address::Immediate(vec![BOOL_TRUE]))
+                }
+                else {
+                    Ok(Address::Immediate(vec![BOOL_FALSE]))
+                }
+            },
+            Literal::Int(value) => {
+                if *value == 0 {
+                    Ok(Address::Immediate(vec![BOOL_FALSE]))
+                }
+                else {
+                    Ok(Address::Immediate(vec![BOOL_TRUE]))
+                }
+            },
+            other => {
+                create_literal_not_impl_error(other, self.get_type_symbol())
+            }
+        }
+
+    }
+
     default_type_operate_impl!(BoolType);
 }
 
