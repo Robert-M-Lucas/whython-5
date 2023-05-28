95434d50245e3eb98d1eca966de766de8ac9813b
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-28 17:30:30 +0100

    Formatting changes

--- src/processing/types/defaults.rs
+++ src/processing/types/defaults.rs
@@ -3,12 +3,12 @@ macro_rules! default_type_wrapper_struct_and_impl {
     ($wrapper_name: ident, $type_name: ident) => {
         pub struct $wrapper_name {}
 
-        impl crate::processing::types::UninstantiatedType for $wrapper_name {
-            fn instantiate(&self) -> Box<dyn crate::processing::types::Type> {
+        impl $crate::processing::types::UninstantiatedType for $wrapper_name {
+            fn instantiate(&self) -> Box<dyn $crate::processing::types::Type> {
                 Box::new($type_name::new())
             }
 
-            fn get_type_symbol(&self) -> crate::processing::symbols::TypeSymbol {
+            fn get_type_symbol(&self) -> $crate::processing::symbols::TypeSymbol {
                 TypeSymbol::Boolean
             }
         }
@@ -20,8 +20,8 @@ macro_rules! default_type_struct {
     ($type_name: ident) => {
         #[allow(dead_code)]
         pub struct $type_name {
-            operators: Vec<Box<dyn crate::processing::types::Operation<$type_name>>>,
-            address: Option<crate::address::Address>
+            operators: Vec<Box<dyn $crate::processing::types::Operation<$type_name>>>,
+            address: Option<$crate::address::Address>
         }
     };
 }
@@ -40,13 +40,19 @@ macro_rules! default_type_initialiser {
                 }
             }
         }
+
+        impl Default for $type_name {
+            fn default() -> Self {
+                Self::new()
+            }
+        }
     };
 }
 
 #[macro_export]
 macro_rules! default_get_type_symbol_impl {
     ($type_name: ident, $type_symbol: expr) => {
-        fn get_type_symbol(&self) -> crate::processing::symbols::TypeSymbol {
+        fn get_type_symbol(&self) -> $crate::processing::symbols::TypeSymbol {
             $type_symbol
         }
     };
@@ -55,7 +61,7 @@ macro_rules! default_get_type_symbol_impl {
 #[macro_export]
 macro_rules! default_type_operate_impl {
     ($type_name: ident) => {
-        fn operate(&self, rhs: Box<dyn crate::processing::types::Type>) -> Result<(), String> {
+        fn operate(&self, rhs: Box<dyn $crate::processing::types::Type>) -> Result<(), String> {
             for operator in self.operators.iter() {
                 if operator.get_result_type(Some(rhs.get_type_symbol())).is_some() {
                     return operator.operate(self, rhs);
