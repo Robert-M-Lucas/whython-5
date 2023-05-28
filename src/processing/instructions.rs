83154a351ea48f05d8e7b294ac3df98b8c36dff6
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-28 17:31:05 +0100

    Fixed instruction code not being added to memory in default instruction implementation

--- src/processing/instructions.rs
+++ src/processing/instructions.rs
@@ -3,22 +3,23 @@ pub mod stack_up_1;
 pub mod heap_alloc_2;
 pub mod copy_3;
 
-pub type InstructionCode = u16;
+pub type InstructionCodeType = u16;
 pub const INSTRUCTION_CODE_LENGTH: usize = 2;
 
 #[macro_export]
 macro_rules! default_instruction_impl {
     ($name: ident, $caps_name: ident, $code: expr $(, ($arg:ident, $t:ty) )*) => {
-        pub const $caps_name: crate::processing::instructions::InstructionCode = $code;
+        pub const $caps_name: $crate::processing::instructions::InstructionCodeType = $code;
 
         impl $name {
-            pub fn new_alloc(memory_manager: &mut crate::memory::MemoryManager, $($arg: $t),*) -> Self {
+            pub fn new_alloc(memory_manager: &mut $crate::memory::MemoryManager, $($arg: $t),*) -> Self {
                 #[allow(unused_mut)]
-                let mut instruction_memory = Vec::with_capacity(Self::get_size());
+                let mut instruction_memory = Vec::with_capacity(Self::get_size() + $crate::processing::instructions::INSTRUCTION_CODE_LENGTH);
+                instruction_memory.extend($caps_name.to_le_bytes());
                 $(instruction_memory.extend($arg.to_le_bytes());
                 )*
 
-                assert_eq!(instruction_memory.len() - crate::processing::instructions::INSTRUCTION_CODE_LENGTH, Self::get_size());
+                assert_eq!(instruction_memory.len() - $crate::processing::instructions::INSTRUCTION_CODE_LENGTH, Self::get_size());
 
                 let address = memory_manager.append(&instruction_memory);
 
@@ -36,7 +37,7 @@ macro_rules! default_instruction_impl {
             }
         }
 
-        impl crate::processing::instructions::Instruction for $name {
+        impl $crate::processing::instructions::Instruction for $name {
             fn get_address(&self) -> usize {
                 self.address
             }
