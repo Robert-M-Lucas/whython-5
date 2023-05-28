3a3b0ceb2162c7e9a4501369c6df7b8aeacda96e
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-28 17:33:04 +0100

    Added new instructions and updated to not using Instruction::get_size()

--- src/translator.rs
+++ src/translator.rs
@@ -1,10 +1,12 @@
+use crate::processing::instructions::copy_3::{COPY_INSTRUCTION_CODE, CopyInstruction};
+use crate::processing::instructions::heap_alloc_2::{HEAP_ALLOC_INSTRUCTION_CODE, HeapAllocInstruction};
 use crate::processing::instructions::stack_create_0::StackCreateInstruction;
 use crate::processing::instructions::stack_create_0::STACK_CREATE_INSTRUCTION_CODE;
 use crate::processing::instructions::stack_up_1::{STACK_UP_INSTRUCTION_CODE, StackUpInstruction};
 
 macro_rules! translate {
     ($instruction: ident, $data: expr, $i: expr) => {
-        $instruction::get_debug(&$data[$i..$i + $instruction::get_size()], &mut $i)
+        $instruction::get_debug(&$data, &mut $i)
     };
 }
 
@@ -19,9 +21,13 @@ pub fn translate(data: &[u8], translate_one: bool) {
         let code = &data[i..i + 2];
         i += 2;
 
+
+
         let output = match u16::from_le_bytes(code.try_into().unwrap()) {
             STACK_CREATE_INSTRUCTION_CODE => translate!(StackCreateInstruction, data, i),
             STACK_UP_INSTRUCTION_CODE => translate!(StackUpInstruction, data, i),
+            HEAP_ALLOC_INSTRUCTION_CODE => translate!(HeapAllocInstruction, data, i),
+            COPY_INSTRUCTION_CODE => translate!(CopyInstruction, data, i),
             code => panic!("Debug not implemented for code {}", code),
         };
 
