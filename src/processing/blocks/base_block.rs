6e63b7e88e4cad10ef8381577dd44d17500c7528
Robert-App-Dev <robert01.appdev@gmail.com>
2023-05-30 11:13:10 +0100

    Renamed arguments

--- src/processing/blocks/base_block.rs
+++ src/processing/blocks/base_block.rs
@@ -1,6 +1,7 @@
 use crate::memory::MemoryManager;
 use crate::processing::blocks::BlockHandler;
 use crate::processing::instructions::stack_create_0::StackCreateInstruction;
+use crate::processing::instructions::stack_down_4::StackDownInstruction;
 use crate::processing::instructions::stack_up_1::StackUpInstruction;
 use crate::processing::reference_manager::ReferenceStack;
 use crate::processing::symbols::Symbol;
@@ -32,25 +33,26 @@ impl BlockHandler for BaseBlock {
 
     fn on_exit(
         &mut self,
-        memory_managers: &mut MemoryManager,
+        memory_manager: &mut MemoryManager,
         reference_stack: &mut ReferenceStack,
         _symbol_line: &[Symbol],
         current_stack_size: usize,
     ) -> Result<bool, String> {
-        self.on_forced_exit(memory_managers, reference_stack, current_stack_size)?;
+        self.on_forced_exit(memory_manager, reference_stack, current_stack_size)?;
         Ok(true)
     }
 
     fn on_forced_exit(
         &mut self,
-        memory_managers: &mut MemoryManager,
+        memory_manager: &mut MemoryManager,
         _reference_stack: &mut ReferenceStack,
         current_stack_size: usize,
     ) -> Result<(), String> {
         self.stack_create_instruction
             .as_mut()
             .expect("No stack create instruction")
-            .change_stack_size(memory_managers, current_stack_size);
+            .change_stack_size(memory_manager, current_stack_size);
+        StackDownInstruction::new_alloc(memory_manager);
         Ok(())
     }
 }
