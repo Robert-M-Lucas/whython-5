3586a8a033845b6d7b7fe215df75a5a11acf8d96
Robert-App-Dev <robert01.appdev@gmail.com>
2023-06-18 15:48:48 +0100

    Separated stack_memory.rs from runtime_memory.rs

--- src/memory/runtime_memory.rs
+++ src/memory/runtime_memory.rs
@@ -1,3 +1,7 @@
+pub mod stack_memory;
+
+pub use stack_memory::StackMemory;
+
 use std::collections::linked_list::LinkedList;
 use std::fmt::format;
 use std::fs;
@@ -19,99 +23,6 @@ fn dump_bytes(file: &str, data: &Vec<u8>) {
         .open(file).unwrap();
 
     file.write_all(data).unwrap();
-}
-
-pub struct StackMemory {
-    memory: LinkedList<(Vec<u8>, usize)>,
-    current_stack: usize,
-}
-
-impl StackMemory {
-    pub fn new() -> Self {
-        Self {
-            memory: LinkedList::new(),
-            current_stack: 0,
-        }
-    }
-
-    pub fn create_stack(&mut self, size: usize, return_addr: usize) {
-        self.memory.push_back((vec![0; size], return_addr));
-    }
-
-    pub fn index(&self, mut position: usize) -> u8 {
-        for m in self.memory.iter().skip(self.current_stack - 1) {
-            if position >= m.0.len() {
-                position -= m.0.len();
-                continue;
-            }
-            return m.0[position];
-        }
-
-        panic!("Index out of stack!");
-    }
-
-    pub fn index_slice(&self, mut start: usize, mut end: usize) -> &[u8] {
-        for m in self.memory.iter().skip(self.current_stack - 1) {
-            if start >= m.0.len() {
-                start -= m.0.len();
-                end -= m.0.len();
-                continue;
-            }
-            return &m.0[start..end];
-        }
-
-        panic!("Index out of stack!");
-    }
-
-    pub fn get_stack(&self, mut position: usize) -> (&[u8], usize) {
-        for m in self.memory.iter().skip(self.current_stack - 1) {
-            if position >= m.0.len() {
-                position -= m.0.len();
-                continue;
-            }
-            return (&m.0, position);
-        }
-
-        panic!("Index out of stack!");
-    }
-
-    pub fn get_stack_mut(&mut self, mut position: usize) -> (&mut [u8], usize) {
-        for m in self.memory.iter_mut().skip(self.current_stack - 1) {
-            println!("{}", m.0.len());
-            println!("{}", position);
-            if position >= m.0.len() {
-                position -= m.0.len();
-                continue;
-            }
-            return (&mut m.0, position);
-        }
-
-        panic!("Index out of stack!");
-    }
-    
-    pub fn stack_up(&mut self) {
-        self.current_stack += 1;
-    }
-
-    pub fn stack_down_and_delete(&mut self) {
-        self.memory.pop_back().expect("Tried to stack down when there are no stacks!");
-        self.current_stack -= 1;
-    }
-    
-    pub fn get_current_level(&self) -> usize { self.current_stack }
-
-    pub fn dump_bytes(&self, folder_name: &str) {
-        fs::create_dir_all(folder_name).unwrap();
-        for i in self.memory.iter().enumerate() {
-            dump_bytes(format!("{}/{}.bin", folder_name, i.0).as_str(), &(i.1.0));
-        }
-    }
-}
-
-impl Default for StackMemory {
-    fn default() -> Self {
-        Self::new()
-    }
 }
 
 pub struct RuntimeMemoryManager {
