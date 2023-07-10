a9850f98c63b47e32d03e00731fe966ffc62cd48
Robert-App-Dev <robert01.appdev@gmail.com>
2023-07-10 13:35:10 +0100

    Added printdump instruction and line to help debugging

---
+++ src/processing/instructions/print_dump_6.rs
@@ -0,0 +1,56 @@
+use crate::address::Address;
+use crate::default_instruction_impl;
+use crate::memory::{MemoryLocation, MemoryManager, RuntimeMemoryManager};
+use crate::processing::instructions::{Execute, Instruction, INSTRUCTION_CODE_LENGTH, InstructionCodeType};
+use crate::processing::types::Type;
+use crate::util::get_usize;
+
+pub struct PrintDumpInstruction {
+    address: usize
+}
+
+pub const PRINT_DUMP_INSTRUCTION_CODE: InstructionCodeType = 6;
+
+impl PrintDumpInstruction {
+    pub fn new_alloc(memory_manager: &mut MemoryManager, to_dump: &Box<dyn Type>) -> Self {
+        let (address, length) = to_dump.get_address_and_length();
+
+        #[allow(unused_mut)]
+            let mut instruction_memory = Vec::with_capacity(Self::get_size() + INSTRUCTION_CODE_LENGTH);
+        instruction_memory.extend(PRINT_DUMP_INSTRUCTION_CODE.to_le_bytes());
+        instruction_memory.extend(length.to_le_bytes());
+        instruction_memory.extend(address.get_bytes());
+
+        let address = memory_manager.append(&instruction_memory);
+
+        Self { address }
+    }
+
+    pub fn get_size() -> usize {
+        0
+    }
+
+    #[allow(unused_variables)]
+    pub fn get_debug(program_memory: &[u8], pointer: &mut usize) -> String {
+        let size = get_usize(pointer, program_memory);
+        *pointer += Address::get_address_size(program_memory, *pointer, size);
+        stringify!(PrintDumpInstruction).to_string()
+    }
+}
+
+impl Instruction for PrintDumpInstruction {
+    fn get_address(&self) -> usize {
+        self.address
+    }
+}
+
+impl Execute for PrintDumpInstruction {
+    fn execute(memory: &mut RuntimeMemoryManager, _pointer: &mut usize) {
+        let length = get_usize(_pointer, memory.program_memory());
+        let data = Address::evaluate_address_to_data(_pointer, &MemoryLocation::Program, &length, memory);
+        for i in data {
+            print!("{:02X}", i);
+        }
+        println!();
+    }
+}
---
+++ src/processing/lines/printdump.rs
@@ -0,0 +1,47 @@
+use crate::q;
+use crate::memory::MemoryManager;
+use crate::processing::blocks::BlockCoordinator;
+use crate::processing::instructions::dump_5::DumpInstruction;
+use crate::processing::instructions::print_dump_6::PrintDumpInstruction;
+use crate::processing::lines::LineHandler;
+use crate::processing::processor::ProcessingResult;
+use crate::processing::reference_manager::{NamedReference, ReferenceType};
+use crate::processing::symbols::{Keyword, Symbol};
+
+pub struct PrintDumpLine {}
+
+impl LineHandler for PrintDumpLine {
+    fn process_line(
+        line: &[Symbol],
+        memory_manager: &mut MemoryManager,
+        block_coordinator: &mut BlockCoordinator,
+    ) -> ProcessingResult {
+        if line.is_empty() {
+            return ProcessingResult::Unmatched;
+        }
+
+        match line[0] {
+            Symbol::Keyword(Keyword::PrintDump) => {}
+            _ => return ProcessingResult::Unmatched,
+        };
+
+        if line.len() != 2 {
+            return ProcessingResult::Failure("PrintDump must be followed by a variable".to_string());
+        }
+
+        let variable = match &line[1] {
+            Symbol::Name(name) => {
+                match &q!(block_coordinator.get_variable(name.as_str())).reference {
+                    ReferenceType::Variable(variable) => variable,
+                    _ => return ProcessingResult::Failure("PrintDump must be followed by a variable".to_string())
+                }
+
+            }
+            _ => return ProcessingResult::Failure("PrintDump must be followed by a variable".to_string())
+        };
+
+        PrintDumpInstruction::new_alloc(memory_manager, variable);
+
+        return ProcessingResult::Success;
+    }
+}
