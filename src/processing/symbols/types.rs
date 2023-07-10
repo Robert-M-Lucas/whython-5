047fc28fb40c632574fc4f1acc3c59c2358e0b83
Robert-App-Dev <robert01.appdev@gmail.com>
2023-07-10 13:40:16 +0100

    Formatting change

--- src/processing/symbols/types.rs
+++ src/processing/symbols/types.rs
@@ -9,16 +9,23 @@ pub enum TypeSymbol {
     Pointer,
 }
 
+// TODO: Copy this implementation patterns
 pub struct TypeSymbolHandler {}
+
+impl TypeSymbolHandler {
+    pub fn get_raw_symbol(string: &str) -> Option<TypeSymbol> {
+        match string {
+            "int" => Some(TypeSymbol::Integer),
+            "bool" => Some(TypeSymbol::Boolean),
+            "char" => Some(TypeSymbol::Character),
+            "ptr" => Some(TypeSymbol::Pointer),
+            _ => None,
+        }
+    }
+}
 
 impl SymbolHandler for TypeSymbolHandler {
     fn get_symbol(string: &str) -> Option<Symbol> {
-        match string {
-            "int" => Some(Symbol::Type(TypeSymbol::Integer)),
-            "bool" => Some(Symbol::Type(TypeSymbol::Boolean)),
-            "char" => Some(Symbol::Type(TypeSymbol::Character)),
-            "ptr" => Some(Symbol::Type(TypeSymbol::Pointer)),
-            _ => None,
-        }
+        Some(Symbol::Type(TypeSymbolHandler::get_raw_symbol(string)?))
     }
 }
